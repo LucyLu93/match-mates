28686e53098d955e6c91203a95ebd4f04d33b0df
Lucy Mahon <lucyamahon@gmail.com>
2023-08-28 21:26:54 +0100

    Started bootstrap. Chaos has commenced

--- client/src/components/Profiles.jsx
+++ client/src/components/Profiles.jsx
@@ -32,58 +32,60 @@ function Profiles() {
       
 
     return (
-        <div className='Profiles'>
-            <div className='Container'>
-                <div>
-       </div>
-       </div>
-
-<div>
+ 
     <div className="list">
-      <ul>
-    
-       {users.map ((user, index) => (
-        <div key={index}>
-            <li>
-                Firstname: {user.firstname}
-            </li>
+        <div className="card">
+        <div className="row">
+  <div className="col-md-3 col-lg-4">     
+  <div className="card-body">
+  <div className="cardcontainer">
+  <ul>
+   
+    {users.map ((user, index) => (
+     <div key={index}>
+        <div className="card-text">
+         <li>
+             Firstname: {user.firstname}
+         </li>
+          <li>
+             Lastname: {user.lastname}
+             </li> 
              <li>
-                Lastname: {user.lastname}
-                </li> 
-                <li>
-                    Age: {user.age}
-                </li>
-                <li>
-                    Location: {user.location}
-                </li>
-                <li>
-                   Match Wins: {user.wins}
-                </li>
-                <li>
-                    Match Losses: {user.losses}
-                </li>
-                <li>
-                    Match Draws: {user.draws}
-                </li>
-
-               <li>
-                 Profile picture: <img src={user.imageUrl} />
-                </li> 
-        </div>
-
-       ))}
-       </ul>
-       </div>
-       </div>
-</div>
-           
+                 Age: {user.age}
+             </li>
+             <li>
+                 Location: {user.location}
+             </li>
+             <li>
+                Match Wins: {user.wins}
+             </li>
+             <li>
+                 Match Losses: {user.losses}
+             </li>
+             <li>
+                 Match Draws: {user.draws}
+             </li>
+             </div> 
+            <li>
             
-            
-       
+            <div className="card-img-top">
+              Profile picture: <img src={user.imageUrl} />
+              </div>
+             </li> 
+             
+     </div>
+
+    ))}
+    </ul>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+   
+    )} 
+
         
     
-    );
-
-    }
-
-    export default Profiles
+export default Profiles
