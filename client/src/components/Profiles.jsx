b04d60b3274feb7524fff4b10d34ed48fa6df4c2
annasfe <anna_sfe@hotmail.com>
2023-08-29 11:24:34 +0300

    (anna's changes) cards are showing properly

--- client/src/components/Profiles.jsx
+++ client/src/components/Profiles.jsx
@@ -1,39 +1,62 @@
 import React, { useEffect, useState } from "react";
-import './Profiles.css'
-
-
+import "./Profiles.css";
 
 function Profiles() {
-    const [users, setUsers] = useState([]);
-    useEffect(() => {
-        getUsers()
-      }, []);
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-    const getUsers = async () => {
+  const getUsers = async () => {
     try {
-        let response = await fetch('/api/profile');
-        if (response.ok) {
-            let data = await response.json();
-           setUsers(data);
-           
-           
-        } else {
-            console.log(`Server Error: ${response.status} ${response.statusText}`);
-        }
-
+      let response = await fetch("/api/profile");
+      if (response.ok) {
+        let data = await response.json();
+        setUsers(data);
+      } else {
+        console.log(`Server Error: ${response.status} ${response.statusText}`);
+      }
     } catch (err) {
-        console.log(`Network error: ${err.message}`);
+      console.log(`Network error: ${err.message}`);
     }
-};
-    
+  };
 
-     
-    
-      
+  return (
+    <div className="cardcontainer">
+        {users.map((user) => (
+      <div className="card" key={user.id}>
+        <img className="card-img-top" src="https://i.imgur.com/AwPJZvT.jpg" alt="Card image cap" />
+        <div className="card-body">
+          <h5 className="card-title">{user.firstname} {user.lastname}</h5>
+            <div>
+            <ul className="card-text">
+            <li>
+                 Age: {user.age}
+             </li>
+             <li>
+                 Location: {user.location}
+             </li>
+             <li>
+                Match Wins: {user.wins}
+             </li>
+             <li>
+                 Match Losses: {user.losses}
+             </li>
+             <li>
+                 Match Draws: {user.draws}
+             </li>
+             </ul>
+             </div>
+        </div>
+      </div>
+      ))}
+    </div>
+  );
+}
 
-    return (
- 
-    <div className="list">
+export default Profiles;
+
+/*    <div className="list">
         <div className="card">
         <div className="row">
   <div className="col-md-3 col-lg-4">     
@@ -83,9 +106,4 @@ function Profiles() {
     </div>
     </div>
     </div>
-   
-    )} 
-
-        
-    
-export default Profiles
+    */
